186daf08e0ca4051567148505400eae5210919bd
SrWilly19 <wahc1998@gmail.com>
2023-10-29 16:50:33 +0100

    Ejercicio 19 JS completado

---
+++ Ejercicios JS/19.GettersAndSetters/exercise.js
@@ -0,0 +1,39 @@
+class Person {
+  constructor (firstName, lastName, age){
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
+  }
+  get firstName(){
+    return this._firstName;
+  }
+  set firstName(value){
+    this._firstName = value;
+  }
+
+  get lastName(){
+    return this._lastName;
+  }
+  set lastName(value){
+    this._lastName = value;
+  }
+
+  get age(){
+    return this._age;
+  }
+  set age(value){
+    this._age = age;
+  }
+
+  get fullName(){
+    //Si quisiera añadir la edad solo tendria que poner lo mismo al final ${this._age}.
+    return `${this._firstName} ${this._lastName} `;
+  }
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
