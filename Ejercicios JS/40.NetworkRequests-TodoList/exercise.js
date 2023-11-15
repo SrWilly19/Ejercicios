915a8ff7ac094e9b3368e1c0e1b61b3552ca7274
SrWilly19 <wahc1998@gmail.com>
2023-11-15 11:53:42 +0100

    Ejercicio 40 JS Completo

--- Ejercicios JS/40.NetworkRequests-TodoList/exercise.js
+++ Ejercicios JS/40.NetworkRequests-TodoList/exercise.js
@@ -1,9 +1,31 @@
 
-async function getUser(){
-    let response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-    let userData = await response.json();
-    return userData;
-}
 
- getUser();
+ async function fetchData(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const data = await response.json()
+    
+   
+    data.forEach(element => {
+        const contenedor = document.createElement("div")
+        contenedor.classList.add("container")
+        const userID = document.createElement("h1")
+        userID.classList.add("userID")
+        const titulo = document.createElement("h1")
+        titulo.classList.add("title")
+        const completado = document.createElement("input")
+        completado.classList.add("completed")
+        completado.type = "checkbox"
+        contenedor.appendChild(userID)
+        contenedor.appendChild(titulo)
+        contenedor.appendChild(completado)
+        const {userId, id, title, completed} = element
+        userID.textContent = `userId: ${userId}`
+        titulo.textContent = `${title}`
+        completado.checked = completed
+        const body = document.getElementById("body")
+        body.appendChild(contenedor)
 
+    });
+ }
+
+ fetchData();
