d8598f0e58100cbaa396b4fc5fa2707988b6237a
SrWilly19 <wahc1998@gmail.com>
2023-12-13 22:16:26 +0100

    Ejercicio Components - 3

--- Ejercicios React/my-app/src/App.jsx
+++ Ejercicios React/my-app/src/App.jsx
@@ -1,6 +1,9 @@
 import { HelloWorld } from "./HelloWorld";
 import { Message } from "./Message";
 
+/*Can you use the Hello component more than once in the App component? Si What happens if you do? Si lo copio si volvera a representar mi componente en la pagina
+Can you render the Message component directly within the App component? Si What happens if you do? Se representa el mensaje que tenemos dentro de nuestro componente Message
+Write your answers in a comment*/
 export function App(){
     return(
         <div>
