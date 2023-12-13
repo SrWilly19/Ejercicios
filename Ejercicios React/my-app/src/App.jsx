18575bcbb1c551f79134458257f1a52b5a742b79
SrWilly19 <wahc1998@gmail.com>
2023-12-13 22:14:32 +0100

    Ejercicio Components - 2

--- Ejercicios React/my-app/src/App.jsx
+++ Ejercicios React/my-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { HelloWorld } from "./HelloWorld";
-
+import { Message } from "./Message";
 
 export function App(){
     return(
         <div>
-            <HelloWorld />    
+            <HelloWorld />
+            <Message />    
         </div>
     )
 }
