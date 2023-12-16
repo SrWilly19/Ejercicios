f28ef63ea82a1e51578b9db8613fb13c96ac6d43
SrWilly19 <wahc1998@gmail.com>
2023-12-16 17:51:43 +0100

    Ejercicio State - 2

--- Ejercicios React/my-app/src/Counter.jsx
+++ Ejercicios React/my-app/src/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { CounterDisplay } from "./CounterDisplay"
 
 /*When calling "setter" function to increment the counter, should the parameter be a function or an immediate value? Function Why? 
 La funcion de actualizacion nos garantiza la consistencia y precision del estado, especialmente en situaciones donde las actualizaciones del estado pueden ser asincronas
@@ -7,13 +8,24 @@ export function Counter({initialValue = 0}){
     const [counter, setCounter] = useState(initialValue)
 
     function handleCounterIncrement(){
-        setCounter((c) => c +1)
+        setCounter((c) => c + 1)
     }
+
+    function handleCounterDecrement(){
+        setCounter((c) => c - 1)
+    }
+
+    function handleCounterReset(){
+        setCounter(initialValue);
+    }
+
 
     return(
         <div>
-            <h2>I have  counted to {counter}</h2>
+            <CounterDisplay counter={counter} />
             <button onClick={handleCounterIncrement}>Increment</button>
+            <button onClick={handleCounterDecrement}>Decrement</button>
+            <button onClick={handleCounterReset}>Reset</button>
         </div>
     )
 }
---
+++ Ejercicios React/my-app/src/CounterDisplay.jsx
@@ -0,0 +1,3 @@
+export function CounterDisplay({counter}){
+    return  <h2>I have  counted to {counter}</h2>
+}
