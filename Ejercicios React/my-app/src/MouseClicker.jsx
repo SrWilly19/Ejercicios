411faffa4674bdbeef6c1078ff93820914aec586
SrWilly19 <wahc1998@gmail.com>
2023-12-17 19:56:30 +0100

    Ejercicio handling events - 2

--- Ejercicios React/my-app/src/MouseClicker.jsx
+++ Ejercicios React/my-app/src/MouseClicker.jsx
@@ -1,11 +1,42 @@
 export function MouseClicker(){
 
     function handleButtonClick(event){
-        console.log(event.currentTarget.name)
+        
+        /*How can you prevent the name attribute of the button from being printed to the console when the image is clicked?
+        Yo he utilizado un condicional que mire si el tag es una img y dependiendo de ello que imprima el src de la imagen o el nombre del boton
+        */
+        
+        if(event.target.tagName === 'IMG'){
+            const imageSource = event.currentTarget.querySelector('img').src;
+            console.log("Image Source: ", imageSource )
+        } else{
+            console.log(event.currentTarget.name)
+        }
+        /*Obtener el boton que tienne el manejador de eventos
+        const buttonElement = event.currentTarget;
+        //console.log(event.currentTarget.name)
+        
+        //Obtener la imagen dentro del boton
+        const imageElement = buttonElement.querySelector('img'); 
+
+        //verificar si el clic  provino directamente de la imagen
+        if(event.target.tagName === 'IMG'){
+            const imageSource = event.target.src;
+            console.log("Image Source: ", imageSource)
+        }else {
+            //Verificar que la imagen existe antes de acceder a su atributo src
+            const imageSource = imageElement ? imageElement.src : null ;
+            console.log("Image Source: ", imageSource)
+        }*/
     }
+
     return (
         <div>
-            <button name="one" onClick={handleButtonClick}>Click me!</button>
+            
+            <button name="one" onClick={handleButtonClick}>
+                <img src="path-to-your-image.jpg" width={24} height={24} />
+                Click me!
+            </button>
         </div>
     )
 }
