b392f59ed59a0e296dc86b84cc076a2a8c6e7532
SrWilly19 <wahc1998@gmail.com>
2023-12-19 21:28:26 +0100

    Ejercicio Uncontrolled Forms - 2

--- Ejercicios React/my-app/src/UncontrolledLogin.jsx
+++ Ejercicios React/my-app/src/UncontrolledLogin.jsx
@@ -13,6 +13,10 @@ export function UncontrolledLogin(){
     
     }
 
+    /*What are the advantages? La API da un metodo mucho mas sencillo para recopilar los datos de los formularios. Si el formulario contiene campos de archivo, FormData maneja automaticamente
+    la codificacion y envio de estos archivos. Funciona bien con varios tipos, campos de texto, casillas de verificacion, botones de opcion y campos de archivo.
+    What are the disadvantages? Se limita a formularios HTML, su utilidad se enfoca en el manejo de datos de formularios web. No es la mejor opcion si se necesita enviar datos estructurados 
+    de manera mas compleja. No maneja automaticamente datos mas complejos, Es posible que necesites realizar conversiones manuales si tus datos son mas complejos que simples formularios*/
     return (
         <form onSubmit={handleFormSubmit}>
             <h1>My uncontrolled Form</h1>
