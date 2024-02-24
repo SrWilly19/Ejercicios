ff007349e372928c9f49282f1fb8b0b2e5a940f4
SrWilly19 <wahc1998@gmail.com>
2024-02-24 19:41:07 +0100

    Custom Hooks - 3 Completado

--- Ejercicios React/my-app/src/GithubUser.jsx
+++ Ejercicios React/my-app/src/GithubUser.jsx
@@ -1,31 +1,17 @@
-import { useEffect, useState } from "react";
+
+import { useGithubUser } from "./useGithubUser"
+
 
 export function GithubUser({username}){
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null);
+    const {data, loading, error, onFetchUser} = useGithubUser(username);
 
-    async function fecthGithubUser(username){
-        setLoading(true);
-        setError(null);
-        try{
-            const response = await fetch(`https://api.github.com/users/${username}`)
-            const json = await response.json();
-            setData(json); 
-        }catch(error){
-            setError(error)
-            setData(null)
-        }finally{
-            setLoading(false)
-        }
+    function handleGetUserData(){
+        onFetchUser(username)
     }
-
-    useEffect(() => {
-        fecthGithubUser(username) 
-    }, [username])
 
     return (
         <div>
+            <button onClick={handleGetUserData}>Load User Data</button>
             {loading && <h1>Loading...</h1>}
             {error && <h1>There gas been an error</h1>}
             {data && (
